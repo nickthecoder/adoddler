8b8c1102faff79eccb5d011104a26db8ae14ac2a
Nick Robinson <nickthecoder@gmail.com>
2016-07-02 14:07:53 +0100

    Debugging pause/resume, adding prints, and refactoring code.

--- adoddler/printer/printJob.py
+++ adoddler/printer/printJob.py
@@ -75,37 +75,53 @@ class PrintJob( Thread ) :
         # BUG. We aren't remembering the units (and onPause.gcode sets to metric).
         self.paused_relative = self.extrude_counter.relative
 
+        # Note, this is stopping the MAIN listener, which will be added again when we resume.
+        self.serial_reader.remove_listener( self.listen )
+
         print "~~~ Paused ish"
 
         self.paused_position = None
         self.serial_reader.add_listener( self.parse_position )
+        # Note, this listener is removed in parse_position
+
         self.send_command( "M114", expect_ok=False ) # Get current position.
-        self.serial_reader.remove_listener( self.listen )
 
 
     def resume( self ) :
+        
+        # The main listener was removed in the pause method, so add it back
         self.serial_reader.add_listener( self.listen )
+        
         self.send_command( "G90" ) # Absolute positioning
+        print "Now using absolute positioning"
+        
         if self.paused_position is not None :
             x = self.paused_position[0]
             y = self.paused_position[1]
             z = self.paused_position[2]
+            print "Paused position : ", x, y, z
             if x is not None and y is not None and z is not None :
                 self.send_command( "G0 X" + str(x) + " Y" + str(y) + " Z" + str(z) )
+                print "Reset print head position"
 
+        # Make doubly sure that the listener added by the pause method is remove
+        # It should have removed itself when it parsed the position.
+        self.serial_reader.remove_listener( self.parse_position )
 
         if self.paused_relative :
+            print "Reset to absolute positioning"
             self.send_command( "G91" ) # Set to relative positioning
 
-        # BUG. If the job was not using metric units, we need to switch back, as onPause.goce sets to metric.
+        # BUG. If the job was not using metric units, we need to switch back, as onPause.gcode sets to metric.
 
         # If we have manually extruded extra filament during pause, then set the value back,
         # so that extra filament isn't counted for the remainder of the print.
         if self.paused_position and self.paused_position[3] is not None :
             print "~~~Setting extrusion position to", self.paused_position[3]
             self.send_command( "G92 E" + str( self.paused_position[3] ) )
+            print "Reset extrude counter"
 
-
+        print "Resume complete"
         self.paused = False
 
 
@@ -119,6 +135,7 @@ class PrintJob( Thread ) :
 
 
     def parse_position( self, line ) :
+        print "parse_position line :", line
         if line.startswith( "X:") :
             x = None
             y = None
@@ -137,6 +154,7 @@ class PrintJob( Thread ) :
                     e = float( subs[1] )
             self.paused_position = ( x, y, z, e )
             print "~~~Pause found position", self.paused_position
+
             # We have found what we need, now we can remove ourselves
             self.serial_reader.remove_listener( self.parse_position )
 
